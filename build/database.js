81b89c8f4f3c89fc6f28e5f4150cff765dbd404f
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-24 15:33:56 -0500

    Config basedatos 1

--- build/database.js
+++ build/database.js
@@ -4,7 +4,7 @@ var _mongoose = _interopRequireDefault(require("mongoose"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_mongoose["default"].connect("mongodb+srv://".concat(process.env.USER, ":").concat(process.env.PASSWORD, "@cluster0.u8djw.mongodb.net/").concat(process.env.DBNAME, "?retryWrites=true&w=majority"), {
+_mongoose["default"].connect("mongodb+srv://colapi:X7jT0Qp0WyAcidyX@cluster0.u8djw.mongodb.net/colapi?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: true,
