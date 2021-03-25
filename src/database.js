91a2b55ead2ec946edab5fcc8b2396bda2786513
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-24 20:17:54 -0500

    Prueba

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ymr7x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-    useCreateIndex: true
-}).then(db => console.log('Base de datos conectada')).catch(error => console.log(error))
+// mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ymr7x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: true,
+//     useCreateIndex: true
+// }).then(db => console.log('Base de datos conectada')).catch(error => console.log(error))
 
