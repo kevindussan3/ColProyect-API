e520e88fd57d31b62484135eb0270b38c3cbd9ba
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-25 13:44:48 -0500

    RKS0

--- build/database.js
+++ build/database.js
@@ -4,13 +4,17 @@ var _mongoose = _interopRequireDefault(require("mongoose"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_mongoose["default"].connect("mongodb+srv://".concat(process.env.USER, ":").concat(process.env.PASSWORD, "@cluster0.ymr7x.mongodb.net/").concat(process.env.DBNAME, "?retryWrites=true&w=majority"), {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true
-}).then(function (db) {
-  return console.log('Base de datos conectada');
-})["catch"](function (error) {
-  return console.log(error);
-});
+try {
+  _mongoose["default"].connect("mongodb+srv://".concat(process.env.USER, ":").concat(process.env.PASSWORD, "@cluster0.ymr7x.mongodb.net/").concat(process.env.DBNAME, "?retryWrites=true&w=majority"), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+  }).then(function (db) {
+    return console.log('Base de datos conectada');
+  })["catch"](function (error) {
+    return console.log(error);
+  });
+} catch (error) {
+  console.log(error);
+}
