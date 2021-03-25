0db5493496915872d24884ba67871dd63ce99dac
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-25 14:04:16 -0500

    Verificacion dupli email y identificacion

--- build/routes/auth.routes.js
+++ build/routes/auth.routes.js
@@ -11,12 +11,14 @@ var _express = require("express");
 
 var authCtrl = _interopRequireWildcard(require("../controllers/auth.controller"));
 
+var Verficacion = _interopRequireWildcard(require("../middlewares/verifySignup"));
+
 function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 var router = (0, _express.Router)();
-router.post('/signup', authCtrl.signup);
+router.post('/signup', Verficacion.checkDuplicateIdentificationAndEmail, authCtrl.signup);
 router.post('/signin', authCtrl.signin);
 var _default = router;
 exports["default"] = _default;
