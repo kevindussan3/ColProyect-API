1e53e29932c6682584af5cc84b88c66fbab219f7
Dussan01 <Dussan01@users.noreply.github.com>
2021-03-24 20:32:13 -0500

    Rh

--- build/database.js
+++ build/database.js
@@ -4,12 +4,7 @@ var _mongoose = _interopRequireDefault(require("mongoose"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-_mongoose["default"].connect("mongodb+srv://".concat(process.env.USER, ":").concat(process.env.PASSWORD, "@cluster0.ymr7x.mongodb.net/").concat(process.env.DBNAME, "?retryWrites=true&w=majority"), {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true
-}).then(function (db) {
+_mongoose["default"].connect().then(function (db) {
   return console.log('Base de datos conectada');
 })["catch"](function (error) {
   return console.log(error);
